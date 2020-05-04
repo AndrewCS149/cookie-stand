ff8205e18753829334f5d62177a70214f6abae6d
AndrewCS149 <Andrewcs149@gmail.com>
2020-05-04 15:09:14 -0700

    Add ccustomers() method to all objects

--- js/app.js
+++ js/app.js
@@ -4,38 +4,74 @@ var seattle = {
 
   minCust: 23,
   maxCust: 65,
-  avgCookies: 6.3
+  avgCookies: 6.3,
 
+  customers: function() {
+    var min = this.minCust;
+    var max = this.maxCust;
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
 
 var tokyo = {
 
   minCust: 3,
   maxCust: 24,
-  avgCookies: 1.2
+  avgCookies: 1.2,
 
+  customers: function() {
+    var min = this.minCust;
+    var max = this.maxCust;
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
 
 var dubai = {
 
   minCust: 11,
   maxCust: 38,
-  avgCookies: 3.7
+  avgCookies: 3.7,
 
+  customers: function() {
+    var min = this.minCust;
+    var max = this.maxCust;
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
 
 var paris = {
 
   minCust: 20,
   maxCust: 38,
-  avgCookies: 2.3
+  avgCookies: 2.3,
 
+  customers: function() {
+    var min = this.minCust;
+    var max = this.maxCust;
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
 
 var lima = {
 
   minCust: 2,
   maxCust: 16,
-  avgCookies: 4.6
+  avgCookies: 4.6,
 
+  customers: function() {
+    var min = this.minCust;
+    var max = this.maxCust;
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 };
+
+console.log(seattle.customers());
+console.log(tokyo.customers());
+console.log(dubai.customers());
+console.log(paris.customers());
+console.log(lima.customers());
