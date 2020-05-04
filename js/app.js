15c858806e8ca75589cfba0c28ceb10e3e7f0d5e
AndrewCS149 <Andrewcs149@gmail.com>
2020-05-04 14:36:23 -0700

    Add object literals to app.js

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var seattle = {
+
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3
+
+};
+
+var tokyo = {
+
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2
+
+};
+
+var dubai = {
+
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7
+
+};
+
+var paris = {
+
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3
+
+};
+
+var lima = {
+
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6
+
+};
