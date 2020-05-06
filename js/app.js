5b908028be02788614417484f2ac7de64d281a3a
AndrewCS149 <Andrewcs149@gmail.com>
2020-05-05 17:31:52 -0700

    Add comments

--- js/app.js
+++ js/app.js
@@ -5,8 +5,9 @@ var storeHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am',
   '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'
 ];
 
-var sumArr = [];
-var hourlySales = [];
+
+var sumArr = []; // will hold the daily sales for all stores
+var hourlySales = []; // will hold hourly sales for all stores
 
 function Store(storeName, minCust, maxCust, avgCookies) {
   this.storeName = storeName;
@@ -19,6 +20,8 @@ function Store(storeName, minCust, maxCust, avgCookies) {
 Store.prototype.cookiesPerHr = function () {
   var cookieArr = [];
 
+  // calculate cookies consumed per hour by multiplying the 'avgCookies' by a
+  // randomly generated number.
   for (var i = 0; i < storeHours.length; i++) {
     var custPerHr = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
     cookieArr.push(Math.ceil(custPerHr * this.avgCookies));
@@ -80,28 +83,54 @@ Store.prototype.renderSales = function () {
   tBody.appendChild(tRow);
 };
 
+// Prototype to calculate and display the hourly sums as well as
+// the final daily sum on the sales table.
 Store.prototype.renderTotals = function () {
   var totalSum = 0;
   var tempArr = [];
   var hourlyTotal = [];
+  var len = storeHours.length;
 
-  // for (var i = 0; i < hourlySales.length; i++) {
-  //   totalSum += hourlySales[i];
-  // }
-
-  for (var i = 0; i < hourlySales.length; i += storeHours.length) {
-    tempArr.push(hourlySales.slice(i, i + storeHours.length));
+  // calculate total sum of all hours from all store locations
+  for (var i = 0; i < hourlySales.length; i++) {
+    totalSum += hourlySales[i];
   }
 
-  for (var k = 0; k < storeHours.length; k++) {
+  // split hourly sales array by store sales
+  for (i = 0; i < hourlySales.length; i += len) {
+    tempArr.push(hourlySales.slice(i, i + len));
+  }
+
+  // create an array of the hourly sums
+  for (i = 0; i < len; i++) {
     var tempSum = 0;
+
     for (var j = 0; j < tempArr.length; j++) {
-      tempSum += tempArr[j][k];
+      tempSum += tempArr[j][i];
     }
     hourlyTotal.push(tempSum);
   }
 
-  return hourlyTotal;
+  // create 'Total Sales' data block to sales table
+  var tBody = document.getElementById('salesTable');
+  var tRow = document.createElement('tr');
+  var tableD = document.createElement('td');
+  tableD.textContent = 'Total Sales:';
+  tRow.appendChild(tableD);
+
+  // add hourly sums to sales table row
+  for (i = 0; i < storeHours.length; i++) {
+    tableD = document.createElement('td');
+    tableD.textContent = (hourlyTotal[i]);
+    tRow.appendChild(tableD);
+  }
+
+  // add the total sum of all hours from all store locations
+  tableD = document.createElement('td');
+  tableD.textContent = (totalSum);
+  tRow.appendChild(tableD);
+  tBody.appendChild(tRow);
+
 };
 
 // construct new Store object and test all prototypes
@@ -117,5 +146,4 @@ tokyo.renderSales();
 dubai.renderSales();
 paris.renderSales();
 lima.renderSales();
-
-console.log(Store.prototype.renderTotals());
+Store.prototype.renderTotals();
